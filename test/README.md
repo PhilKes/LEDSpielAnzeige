e78a714f1ceaa6805d59de12fad41f589ce5be47
Phil Key <39240633+philkes@users.noreply.github.com>
2020-06-02 20:43:51 +0200

    Test Sketch für MAX7219 + Transistoren added

---
+++ test/README.md
@@ -0,0 +1,7 @@
+### MAX7219 mit Transistoren für 18V
+#### Testschaltplan
+![Schaltplan](./max7219_high_voltage.png)
+
+ 
+ ### Arduino Nano Testsketch
+ [Sketch](./arduino_max7219_test/arduino_max7219_test.ino)
---
+++ test/arduino_max7219_test/arduino_max7219_test.ino
@@ -0,0 +1,36 @@
+//We always have to include the library
+#include "LedControl.h"
+
+/*
+ Now we need a LedControl to work with.
+ ***** These pin numbers will probably not work with your hardware *****
+ pin 12 is connected to the DataIn 
+ pin 11 is connected to the CLK 
+ pin 10 is connected to LOAD 
+ We have only a single MAX72XX.
+ */
+LedControl lc=LedControl(12,11,10,1);
+
+/* we always wait a bit between updates of the display */
+unsigned long delaytime=250;
+
+void setup() {
+  /*
+   The MAX72XX is in power-saving mode on startup,
+   we have to do a wakeup call
+   */
+  lc.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc.setIntensity(0,15);
+  /* and clear the display */
+  lc.clearDisplay(0);
+  lc.setDigit(0, 0, 8, true);
+
+}
+
+void loop() { 
+    delay(1000);
+    lc.setChar(0, 0,' ', true);
+    delay(1000);
+    lc.setChar(0, 0,'8', true);
+}
